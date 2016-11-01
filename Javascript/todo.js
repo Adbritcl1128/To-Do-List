71eba849f267217c35cb59494880836451bce540
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2016-11-01 15:15:00 -0400

    js file

---
+++ Javascript/todo.js
@@ -0,0 +1,41 @@
+
+(function(){
+
+  document
+  .querySelector('form')
+  .addEventListener('submit', function handleAddItemClick(event) {
+    event.preventDefault();
+    console.log('hello');
+
+
+
+  });
+
+function ListItem 
+
+
+//
+// .querySelector('li')
+// .addEventListener('hover', function handleAddItemClick(event) {
+// event.preventDefault();
+//
+// console.log('hello');
+
+
+
+
+
+
+
+
+
+// $( "td" ).hover(
+//   function() {
+//     $( this ).addClass( "hover" );
+//   }, function() {
+//     $( this ).removeClass( "hover" );
+//   }
+// );
+
+
+})();
